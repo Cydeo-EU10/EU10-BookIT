adb61a9064f9dd43095d4ae52a2439bd21d49e8b
jamaldemir <jamal@cybertekschool.com>
2021-08-28 11:41:49 -0400

    generateToken created

--- src/test/java/com/bookit/step_definitions/ApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/ApiStepDefs.java
@@ -1,29 +1,34 @@
 package com.bookit.step_definitions;
 
+import com.bookit.utilities.BookItApiUtil;
+import com.bookit.utilities.ConfigurationReader;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 
 public class ApiStepDefs {
+    String token;
 
     @Given("I logged Bookit api using {string} and {string}")
-    public void i_logged_Bookit_api_using_and(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void i_logged_Bookit_api_using_and(String email, String password) {
+
+        token = BookItApiUtil.generateToken(email,password);
+
     }
 
     @When("I get the current user information from api")
     public void i_get_the_current_user_information_from_api() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        System.out.println("token = " + token);
     }
 
     @Then("status code should be {int}")
     public void status_code_should_be(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+
     }
 
 }
---
+++ src/test/java/com/bookit/utilities/BookItApiUtil.java
@@ -0,0 +1,25 @@
+package com.bookit.utilities;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class BookItApiUtil {
+
+
+    public static String generateToken(String email,String password){
+        Response response = given().
+                accept(ContentType.JSON)
+                .queryParam("email", email)
+                .queryParam("password", password)
+                .when()
+                .get(ConfigurationReader.get("qa2api.url") + "/sign");
+
+        String token = response.path("accessToken");
+
+        String finalToken ="Bearer "+token;
+
+        return  finalToken;
+    }
+}
