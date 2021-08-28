066a9b4643506a7de89e2583b87efe303b35122c
jamaldemir <jamal@cybertekschool.com>
2021-08-28 11:54:42 -0400

    first scenario done

--- src/test/java/com/bookit/step_definitions/ApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/ApiStepDefs.java
@@ -8,11 +8,13 @@ import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.Assert;
 
 import static io.restassured.RestAssured.*;
 
 public class ApiStepDefs {
     String token;
+    Response response;
 
     @Given("I logged Bookit api using {string} and {string}")
     public void i_logged_Bookit_api_using_and(String email, String password) {
@@ -24,10 +26,21 @@ public class ApiStepDefs {
     @When("I get the current user information from api")
     public void i_get_the_current_user_information_from_api() {
         System.out.println("token = " + token);
+
+        //send a GET request "/api/users/me" endpoint to get current user info
+
+         response = given().accept(ContentType.JSON)
+                .and()
+                .header("Authorization", token)
+                .when()
+                .get(ConfigurationReader.get("qa2api.url") + "/api/users/me");
+
     }
 
     @Then("status code should be {int}")
-    public void status_code_should_be(Integer int1) {
+    public void status_code_should_be(int statusCode) {
+
+        Assert.assertEquals(statusCode,response.statusCode());
 
     }
 
