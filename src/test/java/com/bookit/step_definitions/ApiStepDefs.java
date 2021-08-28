6be895161fbbf85e153832cae49956659789fb58
jamaldemir <jamal@cybertekschool.com>
2021-08-28 11:28:22 -0400

    steps created

--- src/test/java/com/bookit/step_definitions/ApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/ApiStepDefs.java
@@ -1,8 +1,29 @@
 package com.bookit.step_definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 import static io.restassured.RestAssured.given;
 
 public class ApiStepDefs {
 
+    @Given("I logged Bookit api using {string} and {string}")
+    public void i_logged_Bookit_api_using_and(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I get the current user information from api")
+    public void i_get_the_current_user_information_from_api() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("status code should be {int}")
+    public void status_code_should_be(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
 
 }
--- src/test/resources/features/user.feature
+++ src/test/resources/features/user.feature
@@ -1,7 +1,7 @@
 @smoke
 Feature: User Verification
 
-
+  @wip
   Scenario: verify information about logged user
     Given I logged Bookit api using "sbirdbj@fc2.com" and "asenorval"
     When I get the current user information from api
